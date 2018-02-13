bfbab3307050b873d4e282a799943ee34419989c
PUN <pun.san@ascendcorp.com>
2018-02-13 15:03:00 +0700

    Compile result of bable src/app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,11 @@
-// JSX
-// var template = <p>This is JSX from app.js</p>;
-var template = React.createElement('h1', { id: "component" }, "This is React component.");
+'use strict';
 
+// JSX
+var template = React.createElement(
+  'p',
+  null,
+  'This is JSX from app.js with babel compiled.'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
