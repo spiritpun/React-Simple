e399083dc19d82d57abc86cb08aeffdc93ab9187
PUN <pun.san@ascendcorp.com>
2018-02-13 17:55:18 +0700

    Render condition

--- src/app.js
+++ src/app.js
@@ -1,5 +1,48 @@
 // JSX
-var template = <p>This is JSX from app.js with babel compiled.</p>;
+var template = (
+    <div>
+        <h1>Pun</h1>
+        <p>This is JSX from app.js with babel compiled.</p>
+        <select>
+            <option>item1</option>
+            <option>item2</option>
+            <option>item4</option>
+            <option>item5</option>
+            <option>item3</option>
+            <option>item6</option>
+            <option>item7</option>
+            <option>item8</option>
+            <option>item9</option>
+            <option>item10</option>
+        </select>
+    </div>
+);
+
+// Create a templateTwo war JAX expression
+// div
+//      h1 -> Pun
+//      p -> Age: 28
+//      p -> Location: BKK
+//  Render templateTwo instend of template
+
+var user = {
+    name: 'Pun Sankasem',
+    age: 28,
+    location: 'BKK'
+};
+
+var getLocation = (location) => {
+    return location ? <p>Location: {location}</p> : '';
+};
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {user.age && <p>Age: 28</p>}
+        {getLocation(user.location)}
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
