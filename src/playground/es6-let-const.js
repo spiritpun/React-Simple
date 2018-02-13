0dcee1395705be4016a176d04b985deef9f2a5a7
PUN <pun.san@ascendcorp.com>
2018-02-13 17:58:35 +0700

    Add let, const leason

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,13 @@
+var nameVar = 'text';
+var nameVar = 2;
+console.log(`nameVar: ${nameVar}`);
+
+let nameLet = 'let';
+// let nameLet = '';
+// Duplicate declaration "nameLet"
+console.log(`nameLet: ${nameLet}`);
+
+const nameConst = 'const';
+// nameConst = 2;
+// "nameConst" is read-only
+console.log(`nameConst: ${nameConst}`);
